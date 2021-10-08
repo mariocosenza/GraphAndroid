a92a6db73158d03a04d13c2e0841abca35392cb3
Snep <61911701+SnepGaming@users.noreply.github.com>
2021-10-08 20:29:59 +0200

    Implemented MonteCarlo method, modified activity_chart.xml

--- app/src/main/java/it/edu/graficicalcolo/CalcoloApprossimato.java
+++ app/src/main/java/it/edu/graficicalcolo/CalcoloApprossimato.java
@@ -1,11 +1,35 @@
 package it.edu.graficicalcolo;
 
-import android.widget.Spinner;
-
-import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.ViewById;
+import java.util.Random;
 
 public class CalcoloApprossimato {
+    public static double PI_AVG = 0;
+
+    public double findPI (int totalPoint) {
+        double x, y;
+        int inside = 0;
+        Random random = new Random();
+        if (totalPoint > 0)
+        for (int i = 0;  i <  totalPoint; i++) {
+            x = random.nextDouble();
+            y = random.nextDouble();
+            if ((x*x + y*y) < 1) {
+                inside++;
+            }
+        }
+        else {
+            totalPoint = 10000;
+            System.out.println("Invalid Number auto setted to 10000");
+            findPI(totalPoint);
+        }
+        return calculatePI(inside, totalPoint);
+    }
+
+    public double calculatePI (int inside, int totalPoint) {
+        double pi = (4 * inside)/ (double) totalPoint;
+        System.out.println(pi);
+        return PI_AVG = pi;
+    }
 
 
 
--- app/src/main/java/it/edu/graficicalcolo/ChartActivity_.java
+++ app/src/main/java/it/edu/graficicalcolo/ChartActivity_.java
@@ -1,6 +1,7 @@
 package it.edu.graficicalcolo;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,14 +14,23 @@ import org.androidannotations.annotations.ViewById;
 public class ChartActivity_ extends AppCompatActivity {
     @ViewById
     EditText editNumber;
+    @ViewById
+    Button buttonCalc;
+    CalcoloApprossimato ca = new CalcoloApprossimato();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart);
     }
+    public void sendValue () {
+
+
+    }
 
     public int getSpnner() {
         return Integer.parseInt(editNumber.getText().toString());
     }
+
 }
